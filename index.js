2b3f613cebc84785c58047a005ee6dcb1497ec76
Tyler Sheffield <tylersheffield427@gmail.com>
2023-12-04 19:15:32 -0700

    commit

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ let path = require("path");
 let knex = require("knex")({
     client: "pg",
     connection: {
-        host: process.env.RDS_HOSTNAME || "",
-        user: process.env.RDS_USERNAME || "",
-        password: process.env.RDS_PASSWORD || "",
+        host: process.env.RDS_HOSTNAME || "awseb-e-ee3vw2q82p-stack-awsebrdsdatabase-moo0bhesoomr.cf6qafoa0mp0.us-east-2.rds.amazonaws.com",
+        user: process.env.RDS_USERNAME || "ebroot",
+        password: process.env.RDS_PASSWORD || "cougarcruiser",
         database: process.env.RDS_DB_NAME || "",
         port: process.env.RDS_PORT || 5432,
         ssl: process.env.DB_SSL ? { rejectUnauthorized: false } : false
@@ -31,6 +31,20 @@ let knex = require("knex")({
 let bcrypt = require('bcrypt');
 let saltRounds = 10;
 
+function isAuthenticated(req, res, next) {
+    console.log(req.session.user);
+    if (req.session && req.session.user) {
+        console.log(req.session.user);
+        return next(); // User is authenticated, proceed to the next function
+
+    }
+    // User is not authenticated
+    res.redirect('/login');
+}
+
+
+
+
 
 app.use(express.urlencoded({ extended: true }));
 
@@ -40,9 +54,57 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => res.render("index"));
 
+app.get("/respond", (req, res) => {
+    res.render("getResponse");
+}
+);
+
+app.get("/login", (req, res) => res.render("login", { req }));
+
+app.post("/login", (req, res) => {
+    // Extract the username and plain text password from the request
+    const { username, password } = req.body;
+
+    // Retrieve the user's hashed password from the database
+    knex.select('*').from('security').where({ username })
+        .then(users => {
+            if (users.length === 0) {
+                res.status(401).json({ error: 'Invalid username or password' });
+                return;
+            }
+
+            let user = users[0];
+            let hash = users[0].password;
+
+            // Compare the provided password with the stored hash
+            bcrypt.compare(password, hash, (err, result) => {
+                if (err) {
+                    res.status(500).json({ error: 'Error verifying password' });
+                    return;
+                }
+
+                if (result) {
+                    // Login successful
+                    req.session.user = { username: user.username, id: user.student_id };
+                    console.log(req.session.user)
+                    res.redirect('/rides');
+
+                } else {
+                    // Passwords do not match
+                    res.status(401).json({ error: 'Invalid username or password' });
+                }
+            });
+        })
+        .catch(error => {
+            console.error('Error during login:', error);
+            res.status(500).json({ error: error.message || error });
+        });
+});
 
 
+app.get("/signup", (req, res) => res.render("signup"));
 
+app.get("/survey", (req, res) => res.render("getResponse"));
 
 
 
--- public/js/main.js
+++ public/js/main.js
@@ -125,20 +125,20 @@ $(document).ready(function () {
     /*	Menu item highlighting
     /* ========================================================================= */
 
-    jQuery('#nav').singlePageNav({
-        offset: jQuery('#nav').outerHeight(),
-        filter: ':not(.external)',
-        speed: 2000,
-        currentClass: 'current',
-        easing: 'easeInOutExpo',
-        updateHash: true,
-        beforeStart: function () {
-            console.log('begin scrolling');
-        },
-        onComplete: function () {
-            console.log('done scrolling');
-        }
-    });
+    // jQuery('#nav').singlePageNav({
+    //     offset: jQuery('#nav').outerHeight(),
+    //     filter: ':not(.external)',
+    //     speed: 2000,
+    //     currentClass: 'current',
+    //     easing: 'easeInOutExpo',
+    //     updateHash: true,
+    //     beforeStart: function () {
+    //         console.log('begin scrolling');
+    //     },
+    //     onComplete: function () {
+    //         console.log('done scrolling');
+    //     }
+    // });
 
     $(window).scroll(function () {
         if ($(window).scrollTop() > 400) {
@@ -161,7 +161,7 @@ $(document).ready(function () {
 
     $(window).resize(function () {
         'use strict',
-        $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
+            $('#home-slider, #slider, .sl-slider, .sl-content-wrapper').css('height', slideHeight);
     });
 
 
