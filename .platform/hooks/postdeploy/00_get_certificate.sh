d7c0dc8309c4b314b3bb24484630da262ef4777d
uriah_ah <109769619+ukaqm@users.noreply.github.com>
2023-12-05 12:09:40 -0700

    onemorecname

--- .platform/hooks/postdeploy/00_get_certificate.sh
+++ .platform/hooks/postdeploy/00_get_certificate.sh
@@ -2,3 +2,4 @@
 # Place in .platform/hooks/postdeploy directory
 sudo certbot -n -d socialsense.is404.net --nginx --agree-tos --email tbsheff@byu.edu
 sudo certbot -n -d sensesocial.is404.net --nginx --agree-tos --email tbsheff@byu.edu
+sudo certbot -n -d intex2-11.is404.net --nginx --agree-tos --email tbsheff@byu.edu
